368dce757017326006ddc871848b382e4c66756a
Pham Quang Yen <yenpq@ascendcorp.com>
2018-06-12 09:33:37 +0700

    initial commit

---
+++ jdk11/src/main/java/com/yenpq/jdk11/Hello.java
@@ -0,0 +1,15 @@
+package com.yenpq.jdk11;
+
+
+/**
+ * @author brian
+ * @version 1.0
+ * @since 12/06/2018
+ */
+
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
